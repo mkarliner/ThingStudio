0f02f95c3e3c5b11c9f375845a2ba2e0ccde8456
Paul Grieselhuber <paul@loudnoises.us>
2015-03-27 18:10:10 +0100

    sliding menu improvements

--- client/help/side_nav.js
+++ client/help/side_nav.js
@@ -9,11 +9,21 @@ Template.sideNav.events({
 	'click ul#sidebar li.top' : function(e) {		
 		if ($(e.target).parent().hasClass("active")) {
 			console.log("clicked has class, removing active");
-			// $(e.target).parent().removeClass("active");
 		} else {
 			console.log("clicked does not have class, adding active");
 			$(e.target).parent().siblings().removeClass("active");
 			$(e.target).parent().addClass('active');
 		}
+	},
+	'click a.close-menu': function(e) {
+		e.preventDefault();
+		$("#main-navigation").removeClass('open');
 	}
+});
+
+Template.navButton.events({
+	'click a.open-menu' : function(e) {
+		e.preventDefault();
+		$('#main-navigation').addClass('open');
+	}	
 });
