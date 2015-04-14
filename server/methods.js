cab08877631b4c66a90d30fe45c75e8da27aa6b9
Michael Karliner <mike@modern-industry.com>
2015-04-14 10:55:30 +0100

    NEW: Feed count

---
+++ server/methods.js
@@ -0,0 +1,9 @@
+Meteor.methods({
+	foreignConnections: function(){
+		if(isAdmin(this.userId)) {
+			c  =	Connections.find({$and: [{host: {$ne: "mqtt.thingstud.io"}}, {host: {$ne: "mqtt.modern-industry.com"}}]}).fetch();
+			console.log("FC: ", c)
+			return c;
+		}
+	}
+})
