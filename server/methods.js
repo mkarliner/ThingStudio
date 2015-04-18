2fc591c41826188852f3082427fc7034b0610125
Michael Karliner <mike@modern-industry.com>
2015-04-18 07:14:39 +0100

    REMOVE: noise

--- server/methods.js
+++ server/methods.js
@@ -2,7 +2,7 @@ Meteor.methods({
 	foreignConnections: function(){
 		if(isAdmin(this.userId)) {
 			c  =	Connections.find({$and: [{host: {$ne: "mqtt.thingstud.io"}}, {host: {$ne: "mqtt.modern-industry.com"}}]}).fetch();
-			console.log("FC: ", c)
+			// console.log("FC: ", c)
 			return c;
 		}
 	}
