8c82f9bf611c64b361dd1cd1b9fdfb7a50b397f2
Michael Karliner <mike@modern-industry.com>
2015-06-03 12:24:20 +0100

    NEW: templatelist and templateurl helpers.

--- client/helpers/templates.js
+++ client/helpers/templates.js
@@ -2,13 +2,13 @@ Template.templatelist.helpers({
 	template_list: function() {
 		
 		sns =  Screens.find({isWidget: false});
-		console.log("SNS: ", sns)
+		// console.log("SNS: ", sns)
 		return sns;
 	},
 
 });
 
 Template.registerHelper("templateurl", function(){
-		console.log("URL", this)
-		return "/view/screens/" + this._id;
+		// console.log("URL", this)
+		return "/viewer/screen/" + this._id;
 })
