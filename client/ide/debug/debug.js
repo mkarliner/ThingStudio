9c9408d76d7f984bcfd283b592c9938764a9e3ab
Michael Karliner <mike@modern-industry.com>
2015-06-10 09:36:14 +0100

    CHANGE: truncate debug print out to 15 characters. (for objects)

--- client/ide/debug/debug.js
+++ client/ide/debug/debug.js
@@ -12,6 +12,7 @@ Template.debugNew.helpers({
 		return Session.get("runtimeErrors");
 	},
 	payload: function(){
-		return JSON.stringify(this.payload);
+	
+		return JSON.stringify(this.payload).substring(0,15);
 	}
 })
