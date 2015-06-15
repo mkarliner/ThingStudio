8f89b4694416639d36d40260b5b8db29a91a1fa5
Paul Grieselhuber <paul@loudnoises.us>
2015-06-15 11:40:17 +0200

    dashboard content / style

--- client/ide/dashboard/dashboard.js
+++ client/ide/dashboard/dashboard.js
@@ -1,5 +1,25 @@
+Template.DashboardBody.onRendered(function() {
+	$('ul.tabs').tabs();
+	$('.tooltipped').tooltip({delay: 50});
+});
+
+Template.DashboardBody.events({
+	'click .select-app': function(ev) {
+		ev.preventDefault();
+		changeActiveApp(this._id);
+	}
+});
+
+
 Template.DashboardBody.helpers({
 	appslist: function() {
 		return Apps.find().fetch();
-	}
+	},
+	current_app: function(){
+		if(this._id == Session.get("currentAppId")) {
+			return true;
+		} else {
+			return false;
+		}
+	},
 });
