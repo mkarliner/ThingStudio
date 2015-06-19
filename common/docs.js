47f4ff9f9e1c83db60a1dedd435a2428c933a66a
Michael Karliner <mike@modern-industry.com>
2015-06-19 13:56:29 +0100

    MISSING files

---
+++ common/docs.js
@@ -0,0 +1,26 @@
+
+// FS.debug = true;
+//
+// Docs = new FS.Collection("docs", {
+// 	stores: [new FS.Store.FileSystem("docs", {path: "~/twiddle"})]
+// })
+//
+//
+// Docs.allow({
+//   insert: function(userId, doc) {
+//     return true;
+//   },
+//   update: function(userId, doc, fieldNames, modifier) {
+//     return true;
+//   },
+//   remove: function(userId, doc) {
+//     return true;
+//   },
+//   download: function(userId) {
+//     return true;
+//   }
+// });
+
+
+Docs = new Meteor.Collection("docs");
+Docs.remove();
