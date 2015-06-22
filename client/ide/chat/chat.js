c75913cf458992aaafbb119b6d74a1883a1ce926
Michael Karliner <mike@modern-industry.com>
2015-06-22 13:49:05 +0100

    FIX: Broken who function (users do not always have roles, so role.indexOf makes an exception)

--- client/ide/chat/chat.js
+++ client/ide/chat/chat.js
@@ -10,21 +10,16 @@ Template.ChatBody.helpers({
 	chats: function(){
 		return Chats.find({}, {sort: {date: 1}});
 	},
-	who: function(){
-		
+	who: function(){	
 		user = Meteor.users.findOne({_id: this.userid});
-				console.log("ADMIN", this.userid, user)
-		admin = user.roles.indexOf('admin') > -1;
 		if (user._id === Meteor.userId()) {
 			return 'me';
-		} else if (user.roles.indexOf)
-		if(admin) {
-			return 'admin'
-		} else  {
-			return 'other';
-
+		} else if (user.roles && user.roles.indexOf('admin') > -1) {
+			return "admin";
+		} else {
+			return "other"
 		}
-		
+
 	}
 });
 
