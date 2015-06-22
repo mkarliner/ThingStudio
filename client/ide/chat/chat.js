84f708f3f37b9d791e56f8251f2cbaf4a2ea937e
Paul Grieselhuber <paul@loudnoises.us>
2015-06-22 14:28:05 +0200

    CHANGE: more chat styles

--- client/ide/chat/chat.js
+++ client/ide/chat/chat.js
@@ -11,15 +11,18 @@ Template.ChatBody.helpers({
 		return Chats.find({}, {sort: {date: 1}});
 	},
 	who: function(){
+		
 		user = Meteor.users.findOne({_id: this.userid});
 				console.log("ADMIN", this.userid, user)
-		if(user.roles) {
-			admin = user.roles.indexOf('admin') > -1;
-			return 'admin'
-		} else if (user._id == Meteor.userId()) {
-			return 'other';
-		} else {
+		admin = user.roles.indexOf('admin') > -1;
+		if (user._id === Meteor.userId()) {
 			return 'me';
+		} else if (user.roles.indexOf)
+		if(admin) {
+			return 'admin'
+		} else  {
+			return 'other';
+
 		}
 		
 	}
