28d5a96ae41ffe3af314ea3b7ec4c77d5c27dcbe
Paul Grieselhuber <paul@loudnoises.us>
2015-07-01 17:13:00 +0200

    FIX: add materializeRadio again

--- client/ide/forms/app.js
+++ client/ide/forms/app.js
@@ -1,8 +1,8 @@
-// Template.materializeRadio.helpers({
-//   log: function() {
-//     console.dir(this)
-//   }
-// });
+Template.materializeRadio.helpers({
+  log: function() {
+    console.dir(this)
+  }
+});
 
 Template.updateAppsForm.helpers({
   log: function() {
@@ -54,21 +54,21 @@ AutoForm.addInputType("select-radio-inline-M", {
   }
 });
 
-// Template["materializeRadio"].helpers({
-//   atts: function selectedAttsAdjust() {
-//     var atts = _.clone(this.atts);
-//     if (this.selected) {
-//       atts.checked = "";
-//     }
-//     // remove data-schema-key attribute because we put it
-//     // on the entire group
-//     delete atts["data-schema-key"];
-//     return atts;
-//   },
-//   dsk: function dsk() {
-//     return {
-//       "data-schema-key": this.atts["data-schema-key"]
-//     }
-//   }
-// });
-// });
+Template["materializeRadio"].helpers({
+  atts: function selectedAttsAdjust() {
+    var atts = _.clone(this.atts);
+    if (this.selected) {
+      atts.checked = "";
+    }
+    // remove data-schema-key attribute because we put it
+    // on the entire group
+    delete atts["data-schema-key"];
+    return atts;
+  },
+  dsk: function dsk() {
+    return {
+      "data-schema-key": this.atts["data-schema-key"]
+    }
+  }
+});
+});
