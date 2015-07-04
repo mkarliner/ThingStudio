60a714432217a5a8a14f754d89fe23cf7ebd848e
Paul Grieselhuber <paul@loudnoises.us>
2015-07-04 16:20:31 +0200

    FIX: Widget parameter + / - behavior

--- client/ide/forms/widgets.js
+++ client/ide/forms/widgets.js
@@ -1,12 +1,16 @@
 Template.afArrayField.onRendered(function() {
-	$('.autoform-add-item').addClass('btn-floating btn-plus')
-	$('.autoform-add-item').remove('.glyphicon')
-	$('.autoform-add-item').append('<i class="mdi-content-add"></i>')
+	// Runs when loading widget parameters, not when adding new ones
+	$('div.panel button.autoform-add-item').addClass('btn-floating btn-plus')
+	$('div.panel button.autoform-add-item .glyphicon').remove()
+	this.$('div.panel button.autoform-remove-item').append('<i class="mdi-content-remove"></i>')
+	this.$('div.panel button.autoform-add-item').append('<i class="mdi-content-add"></i>')
 });
 
 Template.afObjectField.onRendered(function() {
-	$('.autoform-remove-item').addClass('btn-floating btn-plus')
-	$('.autoform-remove-item').remove('.glyphicon')
-	$('.autoform-remove-item').append('<i class="mdi-content-remove"></i>')
+	// Runs BOTH when loading widget parameters, and adding new ones
+	$('div.panel button.autoform-remove-item').addClass('btn-floating btn-plus')
+	$('div.panel button.autoform-remove-item .glyphicon').remove()
+	$('div.panel button.autoform-remove-item .mdi-content-remove').remove()
+	$('div.panel button.autoform-remove-item').append('<i class="mdi-content-remove"></i>')
 });
 
