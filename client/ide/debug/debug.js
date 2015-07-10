e3d1d57d19d7dc9bce9fc0f76b667c315b9e76d6
Paul Grieselhuber <paul@loudnoises.us>
2015-07-10 11:49:53 +0200

    ADD: debug page

--- client/ide/debug/debug.js
+++ client/ide/debug/debug.js
@@ -1,12 +1,10 @@
-Template.Debug.helpers({
+Template.DebugBody.onRendered(function() {
+	$('ul.tabs').tabs();
+});
+
+Template.DebugBody.helpers({
 	feeds: function() {
 		return Feeds.find();
-	},
-	messages: function(){
-		return Messages.find({});
-	},
-	outbox: function(){
-		return Outbox.find({});
 	},
 	runtimeErrors: function(){
 		return Session.get("runtimeErrors");
@@ -26,3 +24,43 @@ Template.Debug.helpers({
 		}
 	}
 })
+
+Template.DebugSubscribe.helpers({
+	messages: function(){
+		return Messages.find({});
+	},
+	payload: function(){
+		try {
+			jstr = JSON.stringify(this.payload);
+			if(jstr.length > 15) {
+				return jstr.substring(0,15)+"...";
+			} else {
+				return jstr;
+			}
+			
+		}
+		catch(err){
+			return err + " " + this
+		}
+	}
+});
+
+Template.DebugPublish.helpers({
+	outbox: function(){
+		return Outbox.find({});
+	},
+	payload: function(){
+		try {
+			jstr = JSON.stringify(this.payload);
+			if(jstr.length > 15) {
+				return jstr.substring(0,15)+"...";
+			} else {
+				return jstr;
+			}
+			
+		}
+		catch(err){
+			return err + " " + this
+		}
+	}
+});
