9a1ee10661da9fe4730b72f4476e95411b0c2007
Paul Grieselhuber <paul@loudnoises.us>
2015-07-10 14:44:23 +0200

    FIX: Checkboxes - there are now 3

--- client/ide/forms/autoform.js
+++ client/ide/forms/autoform.js
@@ -11,6 +11,21 @@ Template.afCheckbox_mCheckbox.onRendered(function() {
 		return text;
 	}
 	var uniqid = makeid();
-	Template.instance().$("input").attr("id", uniqid)
-	Template.instance().$("label").attr("for", uniqid)
+	Template.instance().$(".mat-check input").attr("id", uniqid)
+	Template.instance().$(".mat-check label").attr("for", uniqid)
+})
+
+Template.afCheckbox_materialiseCheckbox.onRendered(function() {
+	function makeid() {
+		var text = "";
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		for( var i=0; i < 5; i++ ) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	}
+	var uniqid = makeid();
+	Template.instance().$(".mat-check input").attr("id", uniqid)
+	Template.instance().$(".mat-check label").attr("for", uniqid)
 })
