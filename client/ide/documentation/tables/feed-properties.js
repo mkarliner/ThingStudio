860c9ee7bf03ea22e368393d7b58a80edb6197b2
Michael Karliner <mike@modern-industry.com>
2015-07-23 09:11:08 +0100

    CHANGE: More intro to feeds, json key in table

--- client/ide/documentation/tables/feed-properties.js
+++ client/ide/documentation/tables/feed-properties.js
@@ -15,6 +15,11 @@ Tables.FeedProperties = [
 		description: "Wether to use this feed for outgoing messages (publish), or incoming (subscribe)"
 	},
 	{
+		name: "Json Key",
+		type: "String",
+		description: "If incoming messages are JSON objects, the Json Key selects one property (key) to use as the value of the feeds messages. So, if you have a complex object coming in and want to do averaging on just one property of the message use Json Key to select the key and check Do calculations. "
+	},
+	{
 		name: "Keep Journal",
 		'type:': "Boolean",
 		description: "If checked, the last 'n' messages will be kept on a first in, first out basis. This is useful for showing time series data."
