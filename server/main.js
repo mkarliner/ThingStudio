3772a5fc4b17a587cf2c4c9c70af11e93bc3e95e
Michael Karliner <mike@modern-industry.com>
2015-09-29 08:38:50 +0100

    NEW: Thermometer widget

--- server/main.js
+++ server/main.js
@@ -417,7 +417,8 @@ Meteor.startup(function() {
 		"TwoSeventyDial.json",
 		"Joystick.json",
 		"Keypad.json",
-		"DeviceOrientation.json"
+		"DeviceOrientation.json",
+		"Thermometer.json"
 	];
 
 	num = Docs.remove({lastUpdated: null});
