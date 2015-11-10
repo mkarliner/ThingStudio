2d33d5bc55cd33e18f4ccec5fbba5b6b17a90515
Paul Grieselhuber <paul@loudnoises.us>
2015-11-10 16:32:05 +0100

    checkbox fix

--- client/ide/forms/autoform.js
+++ client/ide/forms/autoform.js
@@ -1,9 +1,9 @@
 AutoForm.setDefaultTemplateForType('afCheckbox', 'mCheckbox');
 
-Template.afCheckbox_mCheckbox.onRendered(function() {
-	makeCheckboxID();
-})
-
+// Template.afCheckbox_mCheckbox.onRendered(function() {
+// 	makeCheckboxID();
+// })
+//
 Template.afCheckbox_materialiseCheckbox.onRendered(function() {
 	makeCheckboxID();
 })
