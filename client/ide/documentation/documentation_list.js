a8c8cf55189deea76bfaeba8be5af3416d12cde6
Paul Grieselhuber <paul@loudnoises.us>
2015-11-12 10:25:37 +0100

    remove image from docs

--- client/ide/documentation/documentation_list.js
+++ client/ide/documentation/documentation_list.js
@@ -1,31 +1,9 @@
-Template.OldDocumentationBody.onRendered(function() {
-	$('ul.tabs').tabs();
-});
-
 Template.DocumentationBody.onRendered(function() {
 	$('ul.tabs').tabs();
 });
 
 
-Template.OldDocumentationBody.helpers({
-	docs: function(category){
-		return HelpPages.find({group: category}, {sort: {pagenumber: 1}});
-	},
-	log: function() {
-		console.log(this)
-	},
-	publishedDate: function(date) {
-		return moment(date).format('MMMM D, YYYY');
-	}
-})
-
-
 Template.DocumentationBody.helpers({
-	// docs: function(category){
-	// 	// d =  Docs.find({"attributes.group": category}, {sort: {pagenumber: 1}});
-	// 	console.log("DCS ", d)
-	// 	return d;
-	// },
 	doc: function(title) {
 		d =  Docs.findOne({"attributes.title": title});
 		if(!d) {
@@ -34,8 +12,6 @@ Template.DocumentationBody.helpers({
 		} else {
 			return d;
 		}
-		//console.log("Finding Doc ", title, d)
-		
 	},
 	log: function() {
 		console.log(this)
@@ -63,5 +39,9 @@ Template.DocumentationBody.helpers({
 	},
 	addedremoved: function(){
 		return this.diff.added ? "Addition" : "Removal";
+	},
+	firstLetter: function () {
+		console.log("docs this", this)
+		return this.attributes.title.charAt(0).toLowerCase();
 	}
 })
