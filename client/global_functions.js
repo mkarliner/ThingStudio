4e21482c38cfda95e4eb59085478b9fe4b58cde3
Paul Grieselhuber <paulisloud@pauls-mbp.home>
2015-12-08 16:34:17 +0100

    starting tabbed template list

--- client/global_functions.js
+++ client/global_functions.js
@@ -427,6 +427,34 @@ InitialiseApps = function() {
 	}
 }
 
+InstantiateWidgets = function(){
+	//Instantiate all screens which are widgets
+	wdgts = Widgets.find().fetch();
+	// console.log("Instantiating Widgets", wdgts);
+	for(var s=0; s<wdgts.length; s++){
+		wgt = wdgts[s];
+		scr = Screens.findOne({_id: wgt.baseScreen});
+		if(!scr) {
+			break;
+		}
+		if(Template[scr.title]) {
+			// console.log("Deleting: ", scr.title);
+			delete Template[scr.title]; //Remove the existing template.
+		}
+		// console.log("Compiling ", scr.title);
+		compileTemplate(scr.title, scr.html, scr.js);
+		if(wgt.widgetType == "Web Component") {
+			try {
+				// console.log("Registering widget")
+				Template[scr.title].registerElement(wgt.tagName);
+			}
+			catch(err) {
+				console.log("Problem registering element: "+ wgt.tagName);
+			}
+		}
+	}
+}
+
 getCredentials = function(){
 	return Session.get("currentCredentials");
 }
--- client/ide/templates/templates.js
+++ client/ide/templates/templates.js
@@ -1,6 +1,15 @@
-Template.ScreensBody.created = function(){
-	//InstantiateWidgets();
-}
+Template.ScreensHeader.events({
+	'click li': function (e, tmpl) {
+		var $target = $(e.target)
+		var dataAt = $target.data('template-filter')
+		var li = tmpl.find("ul li.current")
+
+		$(li).removeClass("current")
+		$target.addClass("current")
+
+		console.log(dataAt)
+	}
+})
 
 Template.ScreensBody.helpers({
 	widgetlist: function(){
@@ -10,31 +19,6 @@ Template.ScreensBody.helpers({
 	templatelist: function(){
 		return Screens.find({$or:[{owner: Meteor.userId()}, {isWidget: false} ] }, {sort: {lowercaseTitle: 1}});
 	},
-	// owner: function() {
-	// 	return
-	// },
-	// publicscreens: function() {
-	// 	return Screens.find({public: true})
-	// },
-	// allscreens: function() {
-	// 	return Screens.find({})
-	// },
-	// status: function(){
-	// 	if(this.owner == Meteor.userId()) {
-	// 		return "Owner";
-	// 	} else {
-	// 		u = Meteor.users.findOne(this.owner);
-	// 		return u ? u.username : "Unknown";
-	// 	}
-	// },
-	// fromApp: function(){
-	// 	app = Apps.findOne(this.appId);
-	// 	return app.title;
-	// },
-	// home_page: function(){
-	// 	app = Session.get("currentApp");
-	// 	return  this._id == app.home_page ? "yes" : "no";
-	// },
 	isWidget: function(){
 		return this.isWidget ? "icon-ts-checkmark" : "";
 	},
@@ -42,31 +26,3 @@ Template.ScreensBody.helpers({
 		return( "aa" +this.isWidget)
 	}
 });
-
-InstantiateWidgets = function(){
-	//Instantiate all screens which are widgets
-	wdgts = Widgets.find().fetch();
-	// console.log("Instantiating Widgets", wdgts);
-	for(var s=0; s<wdgts.length; s++){
-		wgt = wdgts[s];
-		scr = Screens.findOne({_id: wgt.baseScreen});
-		if(!scr) {
-			break;
-		}
-		if(Template[scr.title]) {
-			// console.log("Deleting: ", scr.title);
-			delete Template[scr.title]; //Remove the existing template.
-		}
-		// console.log("Compiling ", scr.title);
-		compileTemplate(scr.title, scr.html, scr.js);
-		if(wgt.widgetType == "Web Component") {
-			try {
-				// console.log("Registering widget")
-				Template[scr.title].registerElement(wgt.tagName);
-			}
-			catch(err) {
-				console.log("Problem registering element: "+ wgt.tagName);
-			}
-		}
-	}
-}
