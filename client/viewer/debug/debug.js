80cae44378524e9778d25ff5b15535646277464e
Paul Grieselhuber <paul@loudnoises.us>
2016-02-08 11:26:36 +0100

    factor out debug feed toggle

--- client/viewer/debug/debug.js
+++ client/viewer/debug/debug.js
@@ -13,17 +13,7 @@ Template.DebugBody.events({
 
 Template.DebugSubscribe.events({
 	'click .feed-items li': function ( e, tmpl ) {
-		var items = tmpl.findAll( 'li' )
-		$target = $( e.target.closest( 'li' ) )
-		$targetDetails = $target.find('.details')
-		if ( $target.hasClass( 'expanded' ) ) {
-			$($targetDetails).animate({"height": 0, "margin-top": "-0.5em"}, 200 );
-			$target.removeClass( 'expanded' )
-		} else {
-			// $( items ).removeClass( 'expanded' )
-			$target.addClass( 'expanded' )
-			$($targetDetails).animate({"height": $($targetDetails).get(0).scrollHeight, "margin-top": "0.6em"}, 200 );
-		}
+		toggleDebugFeedView(e, tmpl);
 	}
 })
 
@@ -54,17 +44,7 @@ Template.DebugSubscribe.helpers({
 
 Template.DebugPublish.events({
 	'click .feed-items li': function ( e, tmpl ) {
-		var items = tmpl.findAll( 'li' )
-		$target = $( e.target.closest( 'li' ) )
-
-		if ( $target.hasClass( 'expanded' ) ) {
-			$(".expanded .details").animate({"height": 0, "margin-top": "-0.5em"}, 200 );
-			$target.removeClass( 'expanded' )
-		} else {
-			$( items ).removeClass( 'expanded' )
-			$target.addClass( 'expanded' )
-			$(".expanded .details").animate({"height": $(".expanded .details").get(0).scrollHeight, "margin-top": "0.6em"}, 200 );
-		}
+		toggleDebugFeedView(e, tmpl);
 	}
 })
 
---
+++ client/viewer/viewer_functions.js
@@ -0,0 +1,13 @@
+toggleDebugFeedView = function (e, tmpl) {
+  items = tmpl.findAll( 'li' )
+  $target = $( e.target.closest( 'li' ) )
+  $targetDetails = $target.find( '.details' )
+
+  if ( $target.hasClass( 'expanded' ) ) {
+    $($targetDetails).animate( { "height" : 0, "margin-top" : "-0.5em" }, 200 );
+    $target.removeClass( 'expanded' )
+  } else {
+    $target.addClass( 'expanded' )
+    $( $targetDetails ).animate( { "height" : $( $targetDetails ).get( 0 ).scrollHeight, "margin-top" : "0.6em" }, 200 );
+  }
+}
